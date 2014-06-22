4087f71e14158d6e3b9d2da005b1f091d79628d8
Simon Que <simonque@gmail.com>
2014-06-21 21:49:47 -0700

    Add relative timestamp field

--- src/com/codepath/apps/basictwitter/TweetArrayAdapter.java
+++ src/com/codepath/apps/basictwitter/TweetArrayAdapter.java
@@ -1,11 +1,15 @@
 package com.codepath.apps.basictwitter;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Locale;
 
 import com.codepath.apps.basictwitter.models.Tweet;
 import com.nostra13.universalimageloader.core.ImageLoader;
 
 import android.content.Context;
+import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -35,6 +39,8 @@ public class TweetArrayAdapter extends ArrayAdapter<Tweet> {
 				(TextView) view.findViewById(R.id.tvUserName);
 		TextView tweet_body_field =
 				(TextView) view.findViewById(R.id.tvBody);
+		TextView timestamp_field =
+				(TextView) view.findViewById(R.id.tvTimeAgo);
 		// Set view properties.
 		profile_image.setImageResource(android.R.color.transparent);
 		ImageLoader image_loader = ImageLoader.getInstance();
@@ -43,7 +49,28 @@ public class TweetArrayAdapter extends ArrayAdapter<Tweet> {
 								  profile_image);
 		user_name_field.setText(tweet.getUser().getName());
 		tweet_body_field.setText(tweet.getBody());
+		timestamp_field.setText(getRelativeTimeAgo(tweet.getTimestamp()));
 
 		return view;
+	}
+
+	// Returns relative time label, given an absolute timestamp string.
+	private String getRelativeTimeAgo(String timestamp) {
+		final String TWITTER_FORMAT = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
+		SimpleDateFormat format =
+				new SimpleDateFormat(TWITTER_FORMAT, Locale.ENGLISH);
+		format.setLenient(true);
+		String relative_date = "";
+		try {
+			long date_ms = format.parse(timestamp).getTime();
+			relative_date = DateUtils.getRelativeTimeSpanString(
+					date_ms,
+					System.currentTimeMillis(),
+					DateUtils.SECOND_IN_MILLIS).toString();
+		} catch (ParseException e) {
+			System.err.println(e.getMessage());
+			e.printStackTrace();
+		}
+		return relative_date;
 	}
 }
