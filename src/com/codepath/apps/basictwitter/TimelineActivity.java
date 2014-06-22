4125429f0ed41e8604623da08fe50293ff7c0009
Simon Que <simonque@gmail.com>
2014-06-21 18:34:31 -0700

    Add JSON response handler to Timeline activity

--- src/com/codepath/apps/basictwitter/TimelineActivity.java
+++ src/com/codepath/apps/basictwitter/TimelineActivity.java
@@ -1,13 +1,37 @@
 package com.codepath.apps.basictwitter;
 
+import org.json.JSONArray;
+
+import com.loopj.android.http.JsonHttpResponseHandler;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class TimelineActivity extends Activity {
+	private TwitterClient client;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_timeline);
+
+		client = TwitterApp.getRestClient();
+		populateTimeline();
+	}
+
+	public void populateTimeline() {
+		client.getHomeTimeline(new JsonHttpResponseHandler() {
+			@Override
+			public void onSuccess(JSONArray json) {
+				Log.d("DEBUG", json.toString());
+			}
+
+			@Override
+			public void onFailure(Throwable e, String s) {
+				Log.d("DEBUG", e.toString());
+				Log.d("DEBUG", s.toString());
+			}
+		});
 	}
 }
