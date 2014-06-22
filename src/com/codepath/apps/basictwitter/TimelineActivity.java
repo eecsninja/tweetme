2bea3c33110ca0622558af60f60c0ea6cb0772f2
Simon Que <simonque@gmail.com>
2014-06-21 19:05:55 -0700

    Populate the list view with tweets received from JSON response

--- src/com/codepath/apps/basictwitter/TimelineActivity.java
+++ src/com/codepath/apps/basictwitter/TimelineActivity.java
@@ -1,15 +1,23 @@
 package com.codepath.apps.basictwitter;
 
+import java.util.ArrayList;
+
 import org.json.JSONArray;
 
+import com.codepath.apps.basictwitter.models.Tweet;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class TimelineActivity extends Activity {
 	private TwitterClient client;
+	private ArrayList<Tweet> tweets;
+	private ArrayAdapter<Tweet> tweets_adapter;
+	private ListView tweets_view;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -18,13 +26,19 @@ public class TimelineActivity extends Activity {
 
 		client = TwitterApp.getRestClient();
 		populateTimeline();
+
+		// Load and display tweets.
+		tweets_view = (ListView) findViewById(R.id.lvTweets);
+		tweets = new ArrayList<Tweet>();
+		tweets_adapter = new ArrayAdapter<Tweet>(this, android.R.layout.simple_list_item_1);
+		tweets_view.setAdapter(tweets_adapter);
 	}
 
 	public void populateTimeline() {
 		client.getHomeTimeline(new JsonHttpResponseHandler() {
 			@Override
-			public void onSuccess(JSONArray json) {
-				Log.d("DEBUG", json.toString());
+			public void onSuccess(JSONArray array) {
+				tweets_adapter.addAll(Tweet.fromJSONArray(array));
 			}
 
 			@Override
